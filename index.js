d842b95f9331db868a03dae5d5056fa4ca659628
Paul Bouzakis <github@paulbouzakis.com>
2016-03-11 08:38:08 -0500

    Import di from spak.

---
+++ index.js
@@ -0,0 +1,18 @@
+import SpecRegistration from "./lib/SpecRegistration";
+import CondSpecs from "./lib/CondSpecs";
+import SpecFromFn from "./lib/SpecFromFn";
+import SpecFromClass from "./lib/SpecFromClass";
+import SpecFromValue from "./lib/SpecFromValue";
+import ConfigMod from "./lib/ConfigMod";
+import SpecWithConfigMod from "./lib/SpecWithConfigMod";
+import ActionSpec from "./lib/ActionSpec";
+import HooksSpec from "./lib/HooksSpec";
+import RegistrySpec from "./lib/RegistrySpec";
+import SpecRef from "./lib/SpecRef";
+
+var SpecGroup = SpecRegistration; // Alias for semantics.
+
+export { SpecRegistration, SpecGroup, CondSpecs, SpecRef,
+         SpecFromFn, SpecFromClass, SpecFromValue,
+         ConfigMod, SpecWithConfigMod,
+         ActionSpec, HooksSpec, RegistrySpec };
